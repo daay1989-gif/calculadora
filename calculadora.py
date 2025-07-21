be5f04dae73f605eee02cd631b27959ce519650d
Diego <daay1989@gmail.com>
2025-07-21 15:17:34 -0400

    primera version de la calculadora

---
+++ calculadora.py
@@ -0,0 +1,3 @@
+variable_1 = int(input("Ingrese el primer numero"))
+variable_2 = int(input("Ingrese segundo digito"))
+print(f"la suma de {variable_1} y {variable_2} es: {variable_1 + variable_2}")
