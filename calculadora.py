9691419b35a70e82ab2343de20140542c4696664
Diego <daay1989@gmail.com>
2025-07-21 16:49:22 -0400

    Segunda version. se ale agredo texto en la primera linea

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,4 @@
-variable_1 = int(input("Ingrese el primer numero"))
-variable_2 = int(input("Ingrese segundo digito"))
+#Primera version de calculadora
+variable_1 = int(input("Ingrese el primer numero: "))
+variable_2 = int(input("Ingrese segundo digito: "))
 print(f"la suma de {variable_1} y {variable_2} es: {variable_1 + variable_2}")
